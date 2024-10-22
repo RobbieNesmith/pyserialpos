3ec1b0a8cf1195ae240ac7457951db744e701f57
Robbie Nesmith <bobberto1995@gmail.com>
2024-10-22 07:37:58 -0500

    Rename module

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 
-setup(name="pypos",
+setup(name="pyserialpos",
       version="0.0.0",
       description="module for interacting with POS pole displays",
       author="Robbie Nesmith",
       author_email="bobberto1995@gmail.com",
-      packages=["pypos"],
+      packages=["pyserialpos"],
       install_requires=["pyserial"])
